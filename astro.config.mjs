afc90819bd1a380af96472de618f26defb077fc5
last night <fujizhang2525@gmail.com>
2023-09-27 20:14:15 -0400

    deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  site: 'https://codelastnight.github.io',
+  base: '/livejournal',
   integrations: [mdx(), sitemap(), tailwind()]
 });
