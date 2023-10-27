3a6ab470bfce1bb369c30190f5ffa3557d9404f3
last night <fujizhang2525@gmail.com>
2023-10-27 11:12:53 -0400

    font fix and toc

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,9 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
 import tailwind from "@astrojs/tailwind";
-
-
+import rehypeAutolinkHeadings from 'rehype-autolink-headings';
+import rehypeSlug from 'rehype-slug';
+import rehypeToc from 'rehype-toc';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://codelastnight.github.io',
@@ -12,6 +13,13 @@ export default defineConfig({
   // image: {
   //   service: passthroughImageService(),
   // },
-
+  markdown: {
+    // Applied to .md and .mdx files
+    rehypePlugins: [
+      rehypeAutolinkHeadings,
+      [rehypeSlug, { behavior: 'append' }],
+      [rehypeToc, { headings: ['h1', 'h2'] }],
+    ]
+  },
   integrations: [mdx(), sitemap(), tailwind()]
 });
